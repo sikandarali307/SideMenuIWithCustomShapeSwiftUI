a1090a280e9d987198f065b72892ed944af6fb2a
sikandar <sikandar.ali307@gmail.com>
2021-10-20 12:21:40 +0500

    Initial Commit

---
+++ SideMenuIWithCustomShapeSwiftUI/SideMenuIWithCustomShapeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuIWithCustomShapeSwiftUIApp.swift
+//  SideMenuIWithCustomShapeSwiftUI
+//
+//  Created by Sikandar Ali on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuIWithCustomShapeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
